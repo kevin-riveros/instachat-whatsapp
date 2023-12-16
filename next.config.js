2bc67ac758d22cdf6421ddb6c4dde64e2084bc2f
kevin-riveros <riverosk885@gmail.com>
2023-12-16 03:39:07 -0500

    add next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Resuelve los alias
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+
+    return config;
+  },
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import { cn } from "@/lib/utils"
 
-export const fontSans = Poppins({
+const fontSans = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300",  "400", "500", "600", "700", "800", "900"],
   variable: "--font-sans",
